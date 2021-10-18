585e69c3b2137e4dcedbab3f98fba33bc7b331bb
Conner Chappell <conner.chappell123@gmail.com>
2021-10-18 00:07:08 -0600

    GET todos, POST todo, and GET all todos for a category work

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import todosRoutes from './routes/todos.js'
+
 const app = express()
 const PORT = 8000
 
 app.use(bodyParser.json())
+
+app.use('/todos', todosRoutes)
+
+app.get('/', (req, res) => {
+    res.send('Hello from Homepage.')
+})
 
 app.listen(PORT, () => { 
     console.log(`Server running on port: http://localhost:${PORT}`)
---
+++ routes/todos.js
@@ -0,0 +1,66 @@
+import express from 'express'
+
+const router = express.Router()
+
+// Default todos
+let todos = [
+    {
+        id: 1,
+        title: "Clean",
+        category: "Home",
+        complete: false
+    },
+    {
+        id: 2,
+        title: "Homework",
+        category: "School",
+        complete: false
+    },
+    {
+        id: 3,
+        title: "Laundry",
+        category: "Home",
+        complete: false
+    }
+]
+
+// const findObjectIndex = (arr) => {
+//     const index = arr.findIndex((obj) => {obj.id === id})
+// }
+
+// all routes start with /todos
+
+// GET todos
+router.get('/', (req, res) => {
+    res.send(todos)
+})
+
+// POST adds new todo to todos
+router.post('/add', (req, res) => {
+    const todo = req.body
+    let newId = (todos.length + 1)
+    const newTodo = {id: newId, ...todo, complete: false}
+    todos.push(newTodo)
+    res.send(`Todo item with the title: ${todo.title} was added to the database!`)
+})
+
+// PUT edits todo
+
+
+// DELETE todo
+// router.delete('/delete/:todoId', (req, res) => {
+//     const { todoId }  = req.params
+    
+//     res.send(`Todo with the id: ${todoId} was deleted from the database`)
+// })
+
+// GET todos for a specific category
+router.get('/:category', (req, res) => {
+    const category = req.params.category
+    const foundCategory = todos.filter((todo) => {
+        return todo.category === category
+    })
+    res.send(foundCategory)
+})
+
+export default router
